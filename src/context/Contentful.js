f17559e5cb3eae7f7e41a6d89b856ce28bc68da6
Uzair <uzairhajra76330@gmail.com>
2021-08-10 13:13:00 +0530

    minor changes

--- src/context/Contentful.js
+++ src/context/Contentful.js
@@ -1,5 +1,4 @@
 import * as contentful from "contentful";
-console.log(process.env.REACT_TECH_STORE_SPACE);
 export const client = contentful.createClient({
   space: process.env.REACT_APP_SPACE,
   accessToken: process.env.REACT_APP_ACCESS_TOKEN,
